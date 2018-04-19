adc818bf4dec99f58e69475cb670f53b42c81c7d
EinsteinSu <Einstein.Su@software.dell.com>
2018-04-19 10:55:06 +0800

    first version

---
+++ Source/ExpiredReminder/ExpiredReminder.DataAccess/Case.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpiredReminder.DataAccess
+{
+    public class Case
+    {
+        public int Id { get; set; }
+
+        [MaxLength(20)]
+        public string Accuser { get; set; }
+
+        [MaxLength(20)]
+        public string Accused { get; set; }
+
+        [ForeignKey("Lawyer")]
+        public int LawyerId { get; set; }
+
+        public virtual Lawyer Lawyer { get; set; }
+
+        public string Cause { get; set; }
+
+
+    }
+
+    public class Lawyer
+    {
+        public int Id { get; set; }
+
+        [MaxLength(20)]
+        public string Name { get; set; }
+    }
+}
---
+++ Source/ExpiredReminder/ExpiredReminder/View/GroupedItemsPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ExpiredReminder.View
+{
+    /// <summary>
+    /// Interaction logic for GroupedItemsPage.xaml
+    /// </summary>
+    public partial class GroupedItemsPage : UserControl
+    {
+        public GroupedItemsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
